dc6249109789d7610b18a6f3b740d3b5c6fd640c
ecardenez <imeh-do@outlook.com>
2024-01-23 11:30:07 -0300

    usuario parte 1

--- project/apps/usuario/api.py
+++ project/apps/usuario/api.py
@@ -1,5 +1,6 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from rest_framework.permissions import IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import Usuario
 from .serializers import UsuarioSerializer, UsuarioDetalleSerializer
@@ -11,22 +12,22 @@ from core.permission import CustomModelPermissions
 class UsuarioViewset(viewsets.ReadOnlyModelViewSet):
     queryset = Usuario.objects.all()
     serializer_class = UsuarioSerializer
-    lookup_field = 'id'
     permission_classes = (IsAuthenticated,)
+    filter_backends = (DjangoFilterBackend, filters.OrderingFilter)
     filter_class = UsuarioFilter
-    ordering_fields = ('first_name', 'last_name')
-    ordering = ('id', )
-
-    def get_serializer_class(self):
-        if self.action == 'list':
-            return UsuarioDetalleSerializer
-        return self.serializer_class
+    ordering_fields = ('first_name', 'last_name',)
+    ordering = ('last_name', )
 
     def get_object(self):
         usuario = self.request.user
         if self.kwargs.get('pk') != 'me' and usuario.is_superuser:
             usuario = super().get_object()
         return usuario
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return UsuarioDetalleSerializer
+        return self.serializer_class
 
     def get_permission(self):
         permission_classes = self.permission_classes
--- project/apps/usuario/filters.py
+++ project/apps/usuario/filters.py
@@ -8,10 +8,10 @@ from .models import Usuario
 
 class UsuarioFilter(filters.FilterSet):
     class Meta:
-        models = Usuario
-        fields = ('username',)
+        model = Usuario
+        fields = ('last_name',)
 
-    Filtro = filters.CharFilter(method='buscar_usuario', label='buscar usuario')
+    Filtro = filters.CharFilter(method='buscar_usuario', label='Buscar usuario')
     es_boletin = django_filters.BooleanFilter(field_name='es_boletin')
 
     @staticmethod
